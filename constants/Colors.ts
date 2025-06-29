1c616228e142da8430d31b505de5bc5f4bd0029e
clevermissfox <125201811+clevermissfox@users.noreply.github.com>
2025-06-28 23:53:08 -0700

    Updated Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,19 +4,19 @@ export const lightTheme = {
   primaryDark: "#D4CEC4", // =
   secondary: "#544F45", // =
   secondaryLight: "#655F52", // = 706B58
-  secondaryDark: "#322E26", // =
+  secondaryDark: "#2F2C2A", // =
   accent: "#D85E58", // =
   accentLight: "#E57B76", // =
   accentDark: "#B94C46", // =
   background: "#E0DDD1", // =
   card: "#E9E6DD", // = E9E6DD
-  text: "#322E26", // =
+  text: "#2F2C2A", // =
   textSecondary: "#655F52", // Using secondaryLight =
-  textTertiary: "#8A8984", // Lighter shade of secondary =
+  textTertiary: "#878078", // Lighter shade of secondary =
   border: "#D4CEC4", //  =
   neutral0: "#FFFFFF", //  =
   neutral100: "#F9F8F0", //  =
-  neutral900: "#322E26", //  =
+  neutral900: "#2F2C2A", //  =
   notification: "#BF4A40", // =
   success: "#4CAF50",
   error: "#F44336",
@@ -29,26 +29,24 @@ export const darkTheme = {
   primaryDark: "#D4CEC4", // = B94C46
   secondary: "#544F45", // = 322E26
   secondaryLight: "#655F52", // = 655F52
-  secondaryDark: "#322E26", // = 544F45
+  secondaryDark: "#2F2C2A", // = 544F45
   accent: "#D85E58", // =
   accentLight: "#E57B76", // =
   accentDark: "#B94C46", // =
-  background: "#322E26", // =
-  card: "#706B58", // Slightly lighter than background, but still dark =
+  background: "#2F2C2A", // =
+  card: "#605952", // Slightly lighter than background, but still dark =
   text: "#F9F8F0", // =
   textSecondary: "#E0DDD1", // =
   textTertiary: "#D4CEC4", // =
-  border: "#8A8984", // Using secondaryLight for better contrast =
+  border: "#878078", // Using secondaryLight for better contrast =
   neutral0: "#FFFFFF", //  =
   neutral100: "#F9F8F0", //  =
-  neutral900: "#322E26", //  =
+  neutral900: "#2F2C2A", //  =
   notification: "#BF4A40", // =
   success: "#4CAF50",
   error: "#F44336",
   warning: "#FFC107",
 };
-// F0EEE5 , E9E6DD , F9F8F0 ,
-// 8A8984 , 706B58 ,  655F52 ,  544F45 , 322E26 , 252523 , E0DDD1
 
 export default {
   light: lightTheme,
